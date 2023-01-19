bf4b5e48b2773b7d03b4b4bbe8dd21a3eb37a037
Nicolas Corthorn <nicolas.corthorn@gmail.com>
2023-01-18 19:19:37 -0800

    Add spelling mistake

--- src/portfolio.py
+++ src/portfolio.py
@@ -1,4 +1,4 @@
-# Random optional module. Create as many as you think appropriate.
+# Rendom optional module. Create as many as you think appropriate.
 import pandas as pd
 
 def compute_cumulative_returns(df: pd.DataFrame):
